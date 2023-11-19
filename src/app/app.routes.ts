dde22ac749a8a3b97142b26960a48a5856024f0f
maulanazh15 <maulanazh15@gmail.com>
2023-11-19 21:31:48 +0700

    menambahkan icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: '/mahasiswa',
     pathMatch: 'full',
   },
   {
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'mahasiswa',
+    loadComponent: () => import('./mahasiswa/mahasiswa.page').then( m => m.MahasiswaPage)
   },
 ];
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { IonicModule } from '@ionic/angular';
 
 if (environment.production) {
   enableProdMode();
@@ -14,6 +15,7 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    importProvidersFrom(IonicModule.forRoot({}), HttpClientModule),
     provideIonicAngular(),
     provideRouter(routes),
   ],
